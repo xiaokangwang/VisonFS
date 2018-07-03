47cdbcd8725656e8de49c30efd566271d3a44e8f
Shelikhoo <xiaokangwang@outlook.com>
2018-07-03 15:40:16 +0800

    refine interface

--- file/op.go
+++ file/op.go
@@ -1,13 +1,19 @@
 package file
 
+import "github.com/xiaokangwang/VisonFS/journeldb"
+import "github.com/xiaokangwang/VisonFS/transform"
+
 type FileTree struct {
+	jd journeldb.JournelDB
+	tf transform.Transform
 }
 
-func (ft *FileTree) Ls() {}
+func (ft *FileTree) Ls(path string) {}
 
 //Block=16MB
-func (ft *FileTree) GetFileBlock() {}
-func (ft *FileTree) SetFileBlock() {}
+func (ft *FileTree) GetFileBlock(path string, blockid int) []byte {
+}
+func (ft *FileTree) SetFileBlock(path string, blockid int, content []byte) {}
 
-func (ft *FileTree) Mkdir() {}
-func (ft *FileTree) Rm()    {}
+func (ft *FileTree) Mkdir(path, ele string) {}
+func (ft *FileTree) Rm(path, ele string)    {}
