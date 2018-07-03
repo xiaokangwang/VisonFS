d84a1bf33b52a720c30553bed1980fecc301799a
Shelikhoo <xiaokangwang@outlook.com>
2018-07-03 15:31:07 +0800

    style

--- file/op.go
+++ file/op.go
@@ -8,5 +8,6 @@ func (ft *FileTree) Ls() {}
 //Block=16MB
 func (ft *FileTree) GetFileBlock() {}
 func (ft *FileTree) SetFileBlock() {}
-func (ft *FileTree) Mkdir()        {}
-func (ft *FileTree) Rm()           {}
+
+func (ft *FileTree) Mkdir() {}
+func (ft *FileTree) Rm()    {}
