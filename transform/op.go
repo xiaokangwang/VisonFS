49299633c61cdd6a6b1cb598ac79a0941a4f88eb
Shelikhoo <xiaokangwang@outlook.com>
2018-07-03 15:01:27 +0800

    rs and op forward

--- transform/op.go
+++ transform/op.go
@@ -1,8 +1,15 @@
 package transform
 
+import (
+	"bytes"
+	"strconv"
+)
+
 type Transform struct {
 	GtTCookie string
 	LtTCookie string
+	rs        rsPass
+	ep        encryptPass
 }
 
 /*Transfrom Steps:
@@ -15,13 +22,30 @@ Dup -> gpg encryption
 const Threshold = 1024 * 1024 * 4
 
 func (t *Transform) Advance(f []byte) ([][]byte, string) {
-	transformMethod := "???"
+	var transformMethod string
 	if len(f) > Threshold {
 		transformMethod = t.GtTCookie
+		dlen := len(f)
+		transformMethod += ";" + strconv.Itoa(dlen)
+		out := t.rs.PassForword(f)
+		for k := range out {
+			var transBuffer bytes.Buffer
+			t.ep.PassForword(&transBuffer, bytes.NewBuffer(out[k]))
+			out[k] = transBuffer.Bytes()
+		}
+		return out, transformMethod
 	} else {
 		transformMethod = t.LtTCookie
+		out := make([][]byte, 4)
+		var i = 0
+		for i <= 3 {
+			var transBuffer bytes.Buffer
+			t.ep.PassForword(&transBuffer, bytes.NewBuffer(f))
+			out[i] = transBuffer.Bytes()
+			i++
+		}
+		return out, transformMethod
 	}
-	panic(nil)
 }
 func (t *Transform) Reverse(b [][]byte, c string) []byte {
 	panic(nil)
--- transform/rs.go
+++ transform/rs.go
@@ -1,6 +1,46 @@
 package transform
 
+import (
+	"io"
+
+	"github.com/klauspost/reedsolomon"
+)
+
 type rsPass struct {
 	datashard   int
 	parityshard int
+	enc         reedsolomon.Encoder
+}
+
+func (rp *rsPass) ensureRs() {
+	if rp.enc == nil {
+		var err error
+		rp.enc, err = reedsolomon.New(rp.datashard, rp.parityshard)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+func (rp *rsPass) PassForword(f []byte) [][]byte {
+	rp.ensureRs()
+	ot, err := rp.enc.Split(f)
+	if err != nil {
+		panic(err)
+	}
+	e := rp.enc.Encode(ot)
+	if e != nil {
+		panic(err)
+	}
+	return ot
+}
+func (rp *rsPass) PassReverse(b [][]byte, len int, w io.Writer) {
+	rp.ensureRs()
+	err := rp.enc.ReconstructData(b)
+	if err != nil {
+		panic(err)
+	}
+	err = rp.enc.Join(w, b, len)
+	if err != nil {
+		panic(err)
+	}
 }
