6b0848b9b4846102f268791ca41f49cfd7ea4a37
Shelikhoo <xiaokangwang@outlook.com>
2018-07-03 20:31:31 +0800

    Accu

---
+++ cache/upload.go
@@ -0,0 +1,32 @@
+package cache
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func isExist(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+func IsDirty(path string) bool {
+	return isExist(path + ".dirty")
+}
+func SetDirty(path string) {
+	f, _ := os.Create(path + ".dirty")
+	f.Close()
+}
+func RemoveDirty(path string) {
+	os.Remove(path + ".dirty")
+}
+func Purge(path string) {
+	filepath.Walk(path, func(pathi string, f os.FileInfo, err error) error {
+		if !IsDirty(pathi) && !strings.HasSuffix(pathi, ".dirty") {
+			os.Remove(pathi)
+		}
+		return nil
+	})
+}
--- sync/event.go
+++ sync/event.go
@@ -1,10 +1,19 @@
 package sync
 
-import "github.com/xiaokangwang/VisonFS/journeldb"
-import "github.com/xiaokangwang/VisonFS/transform"
+import (
+	"bytes"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/xiaokangwang/VisonFS/cache"
+	"github.com/xiaokangwang/VisonFS/journeldb"
+	"github.com/xiaokangwang/VisonFS/transform"
+)
 
 type PendingSync struct {
 	cacheDir          string
+	metadomain        string
 	cacheusing        uint64
 	cacheCap          uint64
 	LastUploadMetaRev uint64
@@ -18,6 +27,27 @@ func (ps *PendingSync) BlobUpload(content []byte) string {
 func (ps *PendingSync) BlobGet(hash string) []byte {
 
 }
-func (ps *PendingSync) SyncMeta()                       {}
+func (ps *PendingSync) SyncMeta() {
+	syncto := ps.jd.CurrentRev()
+
+	uploading := ps.LastUploadMetaRev + 1
+	uploadingFirst := uploading
+	var syncbuf bytes.Buffer
+	for uploading <= syncto {
+		ps.jd.CreateRev(uploading, &syncbuf)
+		uploading++
+	}
+
+	fname := ps.cacheDir + "/meta/" + ps.metadomain + "_rev_" + strconv.FormatUint(uploadingFirst, 10)
+	ps.QueueFileNetworkUpload(fname, syncbuf.Bytes())
+
+}
+func (ps *PendingSync) QueueFileNetworkUpload(fname string, content []byte) {
+	cache.SetDirty(fname)
+	f, err := os.Create(fname)
+	io.Copy(f, bytes.NewBuffer(content))
+	//TODO:Queue Upload
+	cache.RemoveDirty(fname)
+}
 func (ps *PendingSync) CheckoutMeta(revlessthan uint64) {}
 func (ps *PendingSync) CreateCheckpoint()               {}
