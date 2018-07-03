3c34a340e625ac15673001fd76f6f95abc3de66b
Shelikhoo <xiaokangwang@outlook.com>
2018-07-03 18:48:24 +0800

    Uploads

--- network/task.go
+++ network/task.go
@@ -2,17 +2,24 @@ package network
 
 type NetworkTaskQueue struct {
 }
+
 type NetworkUploadTask struct {
+	Filename string
+	content  []byte
 }
 type NetworkUploadTaskResult struct {
 }
 type NetworkDownloadTask struct {
+	Filename string
 }
 type NetworkDownloadTaskResult struct {
+	content []byte
 }
 type NetworkListTask struct {
+	Dir string
 }
 type NetworkListTaskResult struct {
+	Files []string
 }
 
 func (ntq *NetworkTaskQueue) EnqueueUploadTask(task NetworkUploadTask)   {}
--- sync/event.go
+++ sync/event.go
@@ -1,10 +1,15 @@
 package sync
 
+import "github.com/xiaokangwang/VisonFS/journeldb"
+import "github.com/xiaokangwang/VisonFS/transform"
+
 type PendingSync struct {
 	cacheDir          string
 	cacheusing        uint64
 	cacheCap          uint64
 	LastUploadMetaRev uint64
+	jd                *journeldb.JournelDB
+	tf                *transform.Transform
 }
 
 func (ps *PendingSync) BlobUpload(content []byte) string {
