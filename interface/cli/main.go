23b0887fd325e9cdd6de8bde8d16ac2990449103
Shelikhoo <xiaokangwang@outlook.com>
2018-07-04 15:54:16 +0800

    outdata

--- interface/cli/main.go
+++ interface/cli/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/gob"
 	"os"
 
 	"github.com/ld9999999999/go-interfacetools"
 	"github.com/nahanni/go-ucl"
 	"github.com/xiaokangwang/VisonFS/conf"
 	"github.com/xiaokangwang/VisonFS/instanceadm"
+	"github.com/xiaokangwang/VisonFS/transfer"
 )
 
 func main() {
@@ -31,8 +33,35 @@ func main() {
 	case "rm":
 		insi.Rm(os.Args[3], os.Args[4])
 	case "push":
+		task := transfer.NewTask(os.Args[3], os.Args[4], true, insi)
+		ProgressTask(task, fi)
 	case "pull":
+		task := transfer.NewTask(os.Args[3], os.Args[4], false, insi)
+		ProgressTask(task, fi)
+	case "resume":
+		f, err := os.Open("resume")
+		if err != nil {
+			panic(err)
+		}
+		o := gob.NewDecoder(f)
+		var Task transfer.Transfer
+		o.Decode(Task)
+		Task.PushFileInstance(insi)
+		ProgressTask(&Task, fi)
 	case "purge":
+		fi.Purge()
+	}
+}
+func ProgressTask(task *transfer.Transfer, fi instanceadm.Instance) {
+	for task.HasNext() {
+		task.ProcessBlock()
+		f, err := os.Create("resume")
+		if err != nil {
+			panic(err)
+		}
+		e := gob.NewEncoder(f)
+		e.Encode(task)
+		f.Close()
 		fi.Purge()
 	}
 }
