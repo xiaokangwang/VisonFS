a719150979bac87670794736e072cdb9a6fc9f8f
Shelikhoo <xiaokangwang@outlook.com>
2018-07-04 13:49:20 +0800

    cliStub

--- interface/cli/main.go
+++ interface/cli/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/ld9999999999/go-interfacetools"
+	"github.com/nahanni/go-ucl"
+	"github.com/xiaokangwang/VisonFS/conf"
+	"github.com/xiaokangwang/VisonFS/instanceadm"
+)
+
+func main() {
+	var conffile conf.Configure
+	cfg, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	p := ucl.NewParser(cfg)
+	result, err := p.Ucl()
+	if err != nil {
+		panic(err)
+	}
+	interfacetools.CopyOut(result, conffile)
+	var fi instanceadm.Instance
+	fi.Prepare(conffile.Gitpath, conffile.Pubdir, conffile.Prvdir, conffile.Prvpass)
+	insi := fi.Launch()
+	switch os.Args[2] {
+	case "mkdir":
+		insi.Mkdir(os.Args[3], os.Args[4])
+	case "rm":
+		insi.Rm(os.Args[3], os.Args[4])
+	case "push":
+	case "pull":
+	}
 }
