fdff6bb33bc3167c86d00794ce478cb96b8ec938
Shelikhoo <xiaokangwang@outlook.com>
2018-07-05 10:21:04 +0800

    added stub file

--- filesystem/fusefs.go
+++ filesystem/fusefs.go
@@ -132,8 +132,13 @@ func (fs *visionFS) Chown(name string, uid uint32, gid uint32, context *fuse.Con
 }
 
 func (fs *visionFS) Truncate(name string, offset uint64, context *fuse.Context) (code fuse.Status) {
+	_, okerr := fs.filei.Attr(name)
+	if okerr != nil {
+		fs.filei.SetSize(name, int64(offset))
+		return fuse.OK
+	}
 
-	return fuse.ENOSYS
+	return fuse.ENOENT
 
 }
 
@@ -198,5 +203,99 @@ func (fs *visionFS) String() string {
 func (fs *visionFS) StatFs(name string) *fuse.StatfsOut {
 
 	return nil
+
+}
+
+type visonFile struct{}
+
+// NewDefaultFile returns a File instance that returns ENOSYS for
+
+// every operation.
+
+func NewDefaultFile() nodefs.File {
+
+	return (*visonFile)(nil)
+
+}
+
+func (f *visonFile) SetInode(*nodefs.Inode) {
+
+}
+
+func (f *visonFile) InnerFile() nodefs.File {
+
+	return nil
+
+}
+
+func (f *visonFile) String() string {
+
+	return "defaultFile"
+
+}
+
+func (f *visonFile) Read(buf []byte, off int64) (fuse.ReadResult, fuse.Status) {
+
+	return nil, fuse.ENOSYS
+
+}
+
+func (f *visonFile) Write(data []byte, off int64) (uint32, fuse.Status) {
+
+	return 0, fuse.ENOSYS
+
+}
+
+func (f *visonFile) Flock(flags int) fuse.Status { return fuse.ENOSYS }
+
+func (f *visonFile) Flush() fuse.Status {
+
+	return fuse.OK
+
+}
+
+func (f *visonFile) Release() {
+
+}
+
+func (f *visonFile) GetAttr(*fuse.Attr) fuse.Status {
+
+	return fuse.ENOSYS
+
+}
+
+func (f *visonFile) Fsync(flags int) (code fuse.Status) {
+
+	return fuse.ENOSYS
+
+}
+
+func (f *visonFile) Utimens(atime *time.Time, mtime *time.Time) fuse.Status {
+
+	return fuse.ENOSYS
+
+}
+
+func (f *visonFile) Truncate(size uint64) fuse.Status {
+
+	return fuse.ENOSYS
+
+}
+
+func (f *visonFile) Chown(uid uint32, gid uint32) fuse.Status {
+
+	return fuse.ENOSYS
+
+}
+
+func (f *visonFile) Chmod(perms uint32) fuse.Status {
+
+	return fuse.ENOSYS
+
+}
+
+func (f *visonFile) Allocate(off uint64, size uint64, mode uint32) (code fuse.Status) {
+
+	return fuse.ENOSYS
 
 }
